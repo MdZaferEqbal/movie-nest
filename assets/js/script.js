860ad59b9f3294b72bc0d14336c1d9ec7614a073
Md Zafer Eqbal <mdzafereqbal@gmail.com>
2025-02-25 15:54:23 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=46ab4985448518009fe1f1d360eda44d&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=46ab4985448518009fe1f1d360eda44d&query=";
+
+const form = document.getElementById('form');
+const query = document.getElementById('query');
+const movieList = document.getElementById('movie-list');
+
+returnMovies(APILINK);
+
+function returnMovies(url) {
+    fetch(url).then(res => res.json()).then(function(data) {
+        console.log(data.results);
+        data.results.forEach(result => {
+            const div_card = document.createElement("div");
+            div_card.classList.add("card");
+
+            const img = document.createElement("img");
+            img.classList.add("thumbnail");
+            img.alt = "Movie Poster";
+            img.src = IMG_PATH + result.poster_path;
+
+            const movieTitle = document.createElement("h3");
+            movieTitle.classList.add("movie-title");
+            movieTitle.innerText = result.title;
+
+            const movieReleaseDate = document.createElement("h5");
+            movieReleaseDate.classList.add("movie-release-date");
+            movieReleaseDate.innerText = result.release_date;
+
+            div_card.appendChild(img);
+            div_card.appendChild(movieTitle);
+            div_card.appendChild(movieReleaseDate);
+
+            movieList.appendChild(div_card);
+        });
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    movieList.innerHTML = "";
+
+    if ( query.value ) {
+        const queryUrl = SEARCHAPI + query.value;
+        returnMovies(queryUrl);
+    } else {
+        returnMovies(APILINK);
+    }
+});
+
+function refreshPage() {
+    location.reload();
+}
